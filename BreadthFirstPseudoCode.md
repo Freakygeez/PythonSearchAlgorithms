88a2bf25aaaf7086d412f16f87e3af3bb79a1855
Freakygeez <freakygeez@protonmail.com>
2024-04-28 12:01:14 +1000

    UPDATED BreadthFirstSearch

--- BreadthFirstPseudoCode.md
+++ BreadthFirstPseudoCode.md
@@ -6,19 +6,16 @@ This type of algorithm works by loading whole layers at a time into the queue, w
 
 Breadth First Search does not use a cost function and may sometimes be referred to as a *uniform cost search*
 
-
     FUNCTION bfs(tree = dictionary, [start , destination] = word or number) Returns path or failure
 
         path <-- an empty list
+        queue <-- FIFO queue containing one level of tree
 
         IF start == destination:
         RETURN error "You are already at your destination"
 
         ELSE IF start NOT IN tree:
         return error "START must exist in the tree"
-
-        current_node <-- START
-        queue <-- FIFO queue containing one level of TREE
 
         WHILE queue NOT empty:
             tree.pop(one dictionary item)
--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -1,49 +1,74 @@
 from time import perf_counter
-searchTree = {
-    'START': ['1', '2'],
-    '1': ['3','4'],
-    '2': ['5', '6'],
-    '3': ['7', '8'],
-    '4': ['9', '10'],
-    '5': ['11', '12'],
-    '6': ['13', '14'],
-    '7': ['FINISH'],
-    '8': [],
-    '9': [],
-    '10': ['15'],
-    '11': [],
-    '12': [],
-    '13': [],
-    '14': ['16'],
-    '15': ['FINISH'],
-    '16': [],
-    'FINISH': []
 
+searchTree = {
+    'Arad': ['Zerind','Sibiu', 'Timisoara'],
+    'Zerind': ['Ordea'],
+    'Ordea': ['Sibiu'],
+    'Timisoara': ['Lugoj'],
+    'Lugoj': ['Mehadia'],
+    'Mehadia': ['Drobeta'],
+    'Drobeta': ['Craiova'],
+    'Craiova': ['Rimmnicu Vicea'],
+    'Sibiu': ['Fagaras','Rimnicu Vicea'],
+    'Fagaras': ['Bucharest'],
+    'Rimnicu Vicea': ['Piesti', 'Craiova'],
+    'Piesti': ['Craiova', 'Bucharest'],
+    'Bucharest': ['Giurgiu', 'Urziceni'],
+    'Giurgiu': [],
+    'Urziceni': ['Hirsova', 'Vaslui'],
+    'Hirsova': ['Eforie'],
+    'Eforie': [],
+    'Vaslui': ['Iasi'],
+    'Iasi': ['Neamt'],
+    'Neamt': []
 }
 
 visited = []
 queue = []
 
-def bfs(visited = [], graph = {}, startNode = any, goalNode=any):
-    visited.append(startNode)
-    queue.append(startNode)
+def bfs(tree = {} , start = str, destination = str):
+    '''
+    The Breadth First Search model is a little abstract and not entirely useful for a route guidance problem,
+    Remember that it ignores cost (i.e. distance) and opens up whole layers at a time, resulting in some illogical looking
+    paths.
+    Whilst the display reads Arad --> Zerid ---->Sibiu (skipping Oradea), it is not skipping Oradea it is the algorithm expanding
+    the 'Arad' layer and exploring its contents and then sequentially moving down to the 'Zerind' layer, exploring it and so on until 
+    the target is found.
+
+    Breadth first searches are good for exhaustive searches on small datasets in a 'is it even here' approach.
+    For anything large, you would want Depth First Search.
+    To do any kind of route guidance, you would want a cost function.
+    
+    '''
+
+    #A timer is used to compare different search algorithms
     t1 = perf_counter()
 
+    if start == destination:
+        Exception("Start destination cannot be the same as Final destination")
+    elif start not in tree:
+        Exception("Start destination must exist in the Tree")
+    
+    path = set()
+    path.add(start)
+    queue.append(start)
+    visited.append(start)
+
     while queue:
-        i = queue.pop(0)
-        print (i, end=" ")
+        node = queue.pop(0)
+        print (node,' --> ', end=" ")
+        
+        for item in tree[node]:
+            path.add(item)
+            if item == destination:
+                t2 = perf_counter()
+                return round((t2-t1)*1000,2)
+            if item not in visited:
+                visited.append(item)
+                queue.append(item)
 
-        for nextNode in graph[i]:
-            if nextNode == goalNode:
-                return print(visited)
-            if nextNode not in visited:
-                visited.append(nextNode)
-                queue.append(nextNode)
-    t2 = perf_counter()
-    print ('time taken: ', round((t2-t1)*1000,2), 'ms')
 
 
+print(bfs(searchTree,'Arad', 'Bucharest'))
 
-bfs(visited,searchTree,'START')
-#result:
-#START 1 2 3 4 5 6 7 8 9 10 11 12 13 14 FINISH
+
