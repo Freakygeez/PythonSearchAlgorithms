72481d190a7d55bb2400d718dee048f3f2e56d6d
Freakygeez <freakygeez@protonmail.com>
2024-05-02 10:36:06 +1000

    ADD AStarSearch code, wip status

--- AStarSearch.py
+++ AStarSearch.py
@@ -1,34 +1,92 @@
 from time import perf_counter
-searchTree = {
-    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
-    'Zerind': {'Ordea':71},
-    'Ordea': {'Sibiu': 151},
-    'Timisoara': {'Lugoj': 111},
-    'Lugoj': {'Mehadia': 70},
-    'Mehadia': {'Drobeta': 75},
-    'Drobeta': {'Craiova': 120},
-    'Craiova': {'Rimmnicu Vicea': 146},
-    'Sibiu': {'Fagaras': 99, 'Rimnicu Vicea': 80},
-    'Fagaras': {'Bucharest': 211},
-    'Rimnicu Vicea': {'Piesti': 97, 'Craiova': 146},
-    'Piesti': {'Craiova':138, 'Bucharest':101},
-    'Bucharest': {'Giurgiu': 90, 'Urziceni': 142},
-    'Giurgiu': {},
-    'Urziceni': {'Hirsova': 98, 'Vaslui': 142},
-    'Hirsova': {'Eforie': 86},
-    'Eforie': {},
-    'Vaslui': {'Iasi': 92},
-    'Iasi': {'Neamt': 87},
-    'Neamt': {}
 
+
+searchTree = {
+    'Arad': ['Zerind','Sibiu', 'Timisoara'],
+    'Zerind': ['Ordea'],
+    'Ordea': ['Sibiu'],
+    'Timisoara': ['Lugoj'],
+    'Lugoj': ['Mehadia'],
+    'Mehadia': ['Drobeta'],
+    'Drobeta': ['Craiova'],
+    'Craiova': ['Rimmnicu Vicea'],
+    'Sibiu': ['Fagaras','Rimnicu Vicea'],
+    'Fagaras': ['Bucharest'],
+    'Rimnicu Vicea': ['Piesti', 'Craiova'],
+    'Piesti': ['Craiova', 'Bucharest'],
+    'Bucharest': ['Giurgiu', 'Urziceni'],
+    'Giurgiu': [],
+    'Urziceni': ['Hirsova', 'Vaslui'],
+    'Hirsova': ['Eforie'],
+    'Eforie': [],
+    'Vaslui': ['Iasi'],
+    'Iasi': ['Neamt'],
+    'Neamt': []
+}
+#Path cost is seperated out into its own dictionary as it becomes much easier to reference
+pathCost = {
+    'Arad': [75, 140, 118],
+    'Zerind': [71],
+    'Ordea': [151],
+    'Timisoara': [111],
+    'Lugoj': [70],
+    'Mehadia': [75],
+    'Drobeta': [120],
+    'Craiova': [146],
+    'Sibiu': [99, 80],
+    'Fagaras': [211],
+    'Rimnicu Vicea': [97, 146],
+    'Piesti': [138, 101],
+    'Bucharest': [90, 142],
+    'Giurgiu': [],
+    'Urziceni': [98, 142],
+    'Hirsova': [86],
+    'Eforie': [],
+    'Vaslui': [92],
+    'Iasi': [87],
+    'Neamt': []
 }
 
-visited = []
 queue = []
+costQueue = []
+
+
+#print(pathCost['Arad'][0])
+
+
+def A_Star_Search(tree, cost, start, destination):
+
+    total_path_cost = 0 
+    path = []
+
+    if start == destination:
+        Exception ("Start is the the same as destination")
+    elif start or destination not in tree:
+        Exception ("start or destination does not exist in the searchTree")
+    
+    while queue:
+        node = queue.pop(-1)
+        nodeCost = costQueue.pop(-1)
+
+        if item == destination:
+            return (path, total_path_cost)
+        
+        for item in tree:
+            item = node
+            if node not in path or cost[nodeCost][0] < costQueue[-1]:
+                path.append(item)
+                queue.append(item)
+                costQueue.append(item)
+                total_path_cost += cost[nodeCost[0]]
+    
+    return Exception ("No path could be found")
+
+
+
+print(A_Star_Search(searchTree,pathCost,'Arad','Bucharest'))
 
 
 
 
-bfs(visited,searchTree,'START')
-#result:
-#START 1 2 3 4 5 6 7 8 9 10 11 12 13 14 FINISH
+
+
