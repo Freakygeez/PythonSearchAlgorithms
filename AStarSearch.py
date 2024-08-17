70fc5c8744303406d5e325cffdaba65bf3e638b7
Freakygeez <143067276+Freakygeez@users.noreply.github.com>
2024-08-17 23:10:27 +1000

    Still WIP - modified to work with json file

--- AStarSearch.py
+++ AStarSearch.py
@@ -1,126 +1,70 @@
 from time import perf_counter
-
-
-searchTree = {
-    'Arad': {
-        'Zerind': 75,
-        'Sibiu': 140, 
-        'Timisoara': 118},
-    
-    'Zerind': {
-        'Ordea': 71},
-
-    'Ordea': {
-        'Sibiu': 151
-              },
-
-    'Timisoara': {
-        'Lugoj': 111
-        },
-
-    'Lugoj': {
-        'Mehadia': 70
-        },
-
-    'Mehadia': {
-        'Drobeta': 75
-                },
-
-    'Drobeta': {
-        'Craiova': 120
-        },
-    'Craiova': {
-        'Rimmnicu Vicea': 146 
-        },
-
-    'Sibiu': {
-        'Fagaras': 99,
-        'Rimnicu Vicea': 80},
-
-    'Fagaras': {
-        'Bucharest': 211},
-
-    'Rimnicu Vicea': {
-        'Piesti': 97, 
-        'Craiova': 146},
-
-    'Piesti': {
-        'Craiova': 138, 
-        'Bucharest': 101},
-
-    'Bucharest': {
-        'Giurgiu': 90, 
-        'Urziceni': 142},
-
-    'Giurgiu': {},
-
-    'Urziceni': {
-        'Hirsova': 98, 
-        'Vaslui': 142},
-
-    'Hirsova': {
-        'Eforie': 86},
-
-    'Eforie': {},
-
-    'Vaslui': {
-        'Iasi': 92},
-
-    'Iasi': {
-        'Neamt': 87},
-
-    'Neamt': {}
-}
+import pandas as pd
 
 queue = []
-cost_queue = []
+reached = []
 
+#print(tree['Arad']['connected']['Zerind']['cost'] )
 
-def A_Star_Search(tree, start, destination):
-
+def A_Star_Search(tree=str, penalty=1000, start = str, destination=str):
+    
+    tree = pd.read_json(tree)
+    t1 = perf_counter()
     total_path_cost = 0 
     path = set()
+    cost_queue = []
 
     if start == destination:
         Exception ("Start is the the same as destination")
     elif start or destination not in tree:
         Exception ("start or destination does not exist in the searchTree")
-    
-    queue.append(start)
-    print(queue)
     path.add(start)
 
-    #Each node may have several nodes, with several distances, attached
-    #So, we append the initial here and then again in a general form later
-    for distance in searchTree[start]:
-        cost_queue.append(searchTree[start][distance])
+    #Load the connecting nodes
+    for node in tree[start]['connected']:
+        queue.append(node)
+        
+    print (queue)
+
+    #Load cost queue
+    for item in queue:
+        cost_queue.append(tree[start]['connected'][item]['cost'])
+    
+    print(cost_queue)
+
+    '''    
+    #for i in tree['Nodes'].loc[(tree[start] < penalty)]:
+        queue.append(i)
+    for i in tree[start].loc[(tree[start] < penalty)]:
+        reached.append(i)
+    total_path_cost = 0
 
     while queue:
-        node = queue.pop(-1)
-        cost = min(cost_queue)
-        cost_queue.remove(cost) #removes by value rather than ID, bit slower though
-        print (node,' --> ', end=" ")
+        #if reached:
+        #    reached.pop(-1)
+        #queue.pop(-1)
+        print (path, '-->', end=' ') # for debug
+        for node in queue:
+            if node == destination:
+                path.add(destination)# skips the end node otherwise 
+                print ((perf_counter()- t1)/1000)
+                return
+            current_node_cost = min(tree[node].loc[(tree[node] < penalty)])
+            if node not in path and current_node_cost < min(reached):
+                cost_queue.append(current_node_cost)
         
-        for item in tree[node]:
-            item = node
-            path.add(item)
-            if item == destination:
-                return (path, total_path_cost)
-            for distance in tree[item]:
-                cost_queue.append(tree[item][distance])
+        total_path_cost += min(cost_queue)
+        
+
             
-            if node not in path:
-                path.append(item)
-                queue.append(item)
-                total_path_cost += cost
+        reached.pop(0)
+        queue.pop(0)
+    '''
+
     
-        return Exception ("No path could be found")
-    
-    return path
 
 
-
-print(A_Star_Search(searchTree,'Arad','Bucharest'))
+print(A_Star_Search('nodes.json', 1000, 'Arad', 'Bucharest'))
 
 
 
